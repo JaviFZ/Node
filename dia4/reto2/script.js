76b6b539c46ed3a1322be7ba2b4736b41285d8ed
JaviFZ <jferzap@gmail.com>
2022-11-12 18:12:27 +0100

    subida dia4 con pokeapp

---
+++ dia4/reto2/script.js
@@ -0,0 +1,73 @@
+let pokeCard = document.querySelector('[data-poke-card]');
+let pokeName = document.querySelector('[data-poke-name]');
+let pokeImg = document.querySelector('[data-poke-img]');
+let pokeImgContainer = document.querySelector('[data-poke-img-container]');
+let pokeId = document.querySelector('[data-poke-id]');
+let pokeTypes = document.querySelector('[data-poke-types]');
+let pokeStats = document.querySelector('[data-poke-stats]');
+
+
+
+function searchPokemon(event) {
+    event.preventDefault();
+    let { value } = event.target.pokemon;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
+        .then(data => data.json())
+        .then(response => PokemonData(response))
+        .catch(err => NotFound())
+}
+
+
+
+
+
+function PokemonData(data) {
+    let sprite =  data.sprites.front_default;
+    let { stats, types } = data;
+
+    pokeName.textContent = data.name;
+    pokeImg.setAttribute('src', sprite);
+    pokeId.textContent = `Nº ${data.id}`;
+    PokemonTypes(types);
+    PokemonStats(stats);
+}
+
+
+
+
+function PokemonTypes(types) {
+    pokeTypes.innerHTML = '';
+    types.forEach(type => {
+        let typeTextElement = document.createElement("div");
+        typeTextElement.textContent = type.type.name;
+        pokeTypes.appendChild(typeTextElement);
+    });
+}
+
+
+
+
+function PokemonStats(stats) {
+    pokeStats.innerHTML = '';
+    stats.forEach(stat => {
+        let statElement = document.createElement("div");
+        let statElementName = document.createElement("div");
+        let statElementAmount = document.createElement("div");
+        statElementName.textContent = stat.stat.name;
+        statElementAmount.textContent = stat.base_stat;
+        statElement.appendChild(statElementName);
+        statElement.appendChild(statElementAmount);
+        pokeStats.appendChild(statElement);
+    });
+}
+
+
+
+function NotFound() {
+    pokeName.textContent = 'No encontrado';
+    pokeImg.setAttribute('src', 'pngjoy.com_pokedex-pokemon-pokedex-hd-png-download_15734846.png');
+    pokeImg.style.background =  '#fff';
+    pokeTypes.innerHTML = '';
+    pokeStats.innerHTML = '';
+    pokeId.textContent = '';
+}
